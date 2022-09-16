a4371adcf781631e04bde756994ec492ece1f554
alejavalerua <105248587+alejavalerua@users.noreply.github.com>
2022-09-16 18:47:53 -0500

    Actualización de tasas de cambio

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,13 +2,15 @@ const currencies = [
   "COP",
   "USD",
   "EUR",
+  "MXN",
 ];
 
 // estructura de datos con la tasa de cambio
 // en cada lista se tiene el valor de la tasa de cambio de la moneda correspondiente
 // contra las otras dos
 final List<List<double>> rates = [
-  [1.0, 0.00026, 0.00025],
-  [3829, 1.0, .94],
-  [4080, 1.07, 1.0]
+  [1.0, 0.00023, 0.00023, 0.0045],
+  [4435.20, 1.0, 1.0, 20.04],
+  [4442.36, 1.0, 1.0, 20.07],
+  [221.34, 0.050, 0.050, 1.0]
 ];
